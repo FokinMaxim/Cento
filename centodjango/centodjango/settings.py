0dcc0d8db920504f935498dde7428942ddf47ee1
FokinMaxim <fokinmaxim2005@gmail.com>
2024-11-08 22:14:02 +0500

    server deployment

--- centodjango/centodjango/settings.py
+++ centodjango/centodjango/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Cento_database',
-        'USER': 'postgres',
+        'USER': 'maxim',
         'PASSWORD': 'vfrtynjc2005',
         'HOST': 'localhost',
         'PORT': '5432',
--- centodjango/main/api.py
+++ centodjango/main/api.py
@@ -5,7 +5,7 @@ from .serializer import  StudentSerializer
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
-    permissions_classes = [
-        permissions.AllowAny
-    ]
+    #permissions_classes = [
+    #    permissions.AllowAny
+    #]
     serializer_class = StudentSerializer
